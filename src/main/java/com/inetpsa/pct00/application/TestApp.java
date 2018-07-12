c537c6ad8a12f688d3d1fd5ead50bead23fb4c04
jan <jan@metafactory.nl>
2018-07-12 08:33:56 +0200

    Added model entities for reading file FIN2PCRT.TXT

--- src/main/java/com/inetpsa/pct00/application/TestApp.java
+++ src/main/java/com/inetpsa/pct00/application/TestApp.java
@@ -1,11 +1,11 @@
 package com.inetpsa.pct00.application;
 
-import com.inetpsa.pct00.application.service.PcFinetImportService;
+import com.inetpsa.pct00.application.service.PcfinetImportService;
 
 public class TestApp {
 
     public static void main(String[] args) {
-        PcFinetImportService p = new PcFinetImportService();
+        PcfinetImportService p = new PcfinetImportService();
 
         p.importRequestsFromFile( "D:\\06 PSA-Projecten\\proposalManagementApplication\\test_data\\FIN2PCRT.VRW" );
     }
