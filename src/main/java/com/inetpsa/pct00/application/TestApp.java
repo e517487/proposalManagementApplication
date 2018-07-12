8c49988c0882b80082d184c6cc0ff59ca657cfbc
jan <jan@metafactory.nl>
2018-07-12 08:33:56 +0200

    Imported Config.wsdl for the WLTP service to  api.yml

--- src/main/java/com/inetpsa/pct00/application/TestApp.java
+++ src/main/java/com/inetpsa/pct00/application/TestApp.java
@@ -1,11 +1,11 @@
 package com.inetpsa.pct00.application;
 
-import com.inetpsa.pct00.application.service.PcfinetImportService;
+import com.inetpsa.pct00.application.service.PcFinetImportService;
 
 public class TestApp {
 
     public static void main(String[] args) {
-        PcfinetImportService p = new PcfinetImportService();
+        PcFinetImportService p = new PcFinetImportService();
 
         p.importRequestsFromFile( "D:\\06 PSA-Projecten\\proposalManagementApplication\\test_data\\FIN2PCRT.VRW" );
     }
